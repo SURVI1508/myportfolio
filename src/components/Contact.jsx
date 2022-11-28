28864a5704bb2f863b5db57de0a50a3882f4b6ea
SOURAV GUPTA <118287650+SURVI1508@users.noreply.github.com>
2022-11-28 11:56:45 +0530

    firebase

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,47 @@
 import React from 'react'
 import './contact.css'
+// import { initializeApp } from 'firebase/app';
+import { useState } from 'react';
 const Contact = () => {
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        message: ""
+    })
+    let name, value;
+    const userData = (e) => {
+        name = e.target.name;
+        value = e.target.value;
+        setUser({ ...user, [name]: value })
+    }
+    const submit = async (e) => {
+        e.preventDefault()
+        const { name, email, message } = user;
+        if (name && email && message) {
+            const res = await fetch('https://portfolio-d0bf4-default-rtdb.firebaseio.com/portfolioForm.json', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name,
+                    email,
+                    message
+                })
+            });
+            if(res){
+                setUser({
+                    name:"",
+                    email:"",
+                    message:""
+                })
+            }
+
+        } else {
+            alert("plx filll data")
+        }
+
+    }
     return (
         <>
             <div className="mainContact__div">
@@ -12,10 +53,10 @@ const Contact = () => {
                             {/* <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d17024.330988246747!2d85.30871238760048!3d23.348924029545156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1668534977774!5m2!1sen!2sin" width="900" height="300" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
 
                             <div className="form__box">
-                                <form action="https://formspree.io/f/mqkjndyd" method='POST'>
-                                    <input type="text" required name='name' placeholder='Enter name' />
-                                    <input type="text" required name='mail' placeholder='Enter mail' />
-                                    <textarea name="message" id="" cols="30" rows="5" placeholder='Write somthing here....'></textarea>
+                                <form onSubmit={submit} >
+                                    <input type="text" name='name' placeholder='Enter name' value={user.name} onChange={userData} method='POST' />
+                                    <input type="text" name='email' placeholder='Enter mail' value={user.email} onChange={userData} />
+                                    <textarea name="message" id="" cols="30" rows="5" placeholder='Write somthing here....' value={user.message} onChange={userData}></textarea>
                                     <input className='message__btn' type="submit" value="SEND" />
                                 </form>
                             </div>
@@ -23,7 +64,6 @@ const Contact = () => {
 
                         </div>
                         <div className="img__box"></div>
-
                     </div>
                 </div>
             </div>
